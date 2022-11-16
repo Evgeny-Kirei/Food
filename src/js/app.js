24e1ebd83fedf1844a5485f8a7d51c244fc69cd2
Evgeny <mishkin-583@mail.ru>
2022-11-16 15:58:38 +0300

    tabs change

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,39 @@
-import * as evFunctions from "./modules/functions.js";
 
-evFunctions.isWebp();
+window.addEventListener('DOMContentLoaded', () =>  {
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+    const tabs = document.querySelectorAll('tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
 
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(0);
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+}); 
