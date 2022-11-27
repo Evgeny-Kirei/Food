193f99f000390108e9a4ab6dc834d93b772d8858
Evgeny <mishkin-583@mail.ru>
2022-11-27 15:54:40 +0300

    tabs comentars dot

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,33 @@
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () =>  {
 
-    const tabs = document.querySelectorAll('tabheader__item'),
+    const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
-
+  
         // Скрываем фото контента
     function hideTabContent() {
         tabsContent.forEach(item => { //Перебирае все фото 
             item.style.display = 'none'; //скрываем
         });
-
+  
         tabs.forEach(item => { //Меняем класс активности
             item.classList.remove('tabheader__item_active');
         });
     }
-
-    function showTabContent(i) { //Показываем фото по очереди 
+  
+    function showTabContent(i = 0) { //Показываем фото по очереди 
         tabsContent[i].style.display = 'block';
         tabs[i].classList.add('tabheader__item_active');
     }
-
+  
     hideTabContent();
-    showTabContent(0);
-
+    showTabContent();
+  
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;  // Добавляем событие клика чтобы при клике менялись фото
-
+  
         if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((item, i) => {
                 if (target == item) {
@@ -36,5 +37,5 @@ window.addEventListener('DOMContentLoaded', () =>  {
             });
         }
     });
-
-}); 
+  
+  }); 
